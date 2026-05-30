4c2cca43dd87e00751f8ed919ca593cd73869143
jzc233 <63482703+jzc233@users.noreply.github.com>
2026-05-30 15:33:16 +0800

    Update and rename twrp_raphael.mk to twrp_perseus.mk

--- twrp_perseus.mk
+++ twrp_perseus.mk
@@ -11,12 +11,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 # Inherit TWRP common configuration
 $(call inherit-product, vendor/twrp/config/common.mk)
 
-# Inherit from raphael device
-$(call inherit-product, device/xiaomi/raphael/device.mk)
+# Inherit from perseus device
+$(call inherit-product, device/xiaomi/perseus/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_DEVICE := raphael
-PRODUCT_NAME := twrp_raphael
+PRODUCT_DEVICE := perseus
+PRODUCT_NAME := twrp_perseus
 PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := Redmi K20 Pro
+PRODUCT_MODEL := Mi Mix 3
 PRODUCT_MANUFACTURER := Xiaomi
